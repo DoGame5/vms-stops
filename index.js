412d74f8b3163ab33264bf868037299f9df6282c
DoGame5 <46245984+DoGame5@users.noreply.github.com>
2024-12-04 00:37:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const exportStops = require("efa-export-stops-by-coordinates");
 
-const ENDPOINT_BASE_URL_TEMPLATE = "http://www.vms.de/vms3/XSLT_COORD_REQUEST?&jsonp=&boundingBox=&boundingBoxLU={minx}%3A{miny}%3AWGS84%5BDD.DDDDD%5D&boundingBoxRL={maxx}%3A{maxy}%3AWGS84%5BDD.DDDDD%5D&coordOutputFormat=WGS84%5BGGZHTXX%5D&type_1=STOP&outputFormat=json&inclFilter=1";
+// DEPRECATED const ENDPOINT_BASE_URL_TEMPLATE = "http://www.vms.de/vms3/XSLT_COORD_REQUEST?&jsonp=&boundingBox=&boundingBoxLU={minx}%3A{miny}%3AWGS84%5BDD.DDDDD%5D&boundingBoxRL={maxx}%3A{maxy}%3AWGS84%5BDD.DDDDD%5D&coordOutputFormat=WGS84%5BGGZHTXX%5D&type_1=STOP&outputFormat=json&inclFilter=1";
+const ENDPOINT_BASE_URL_TEMPLATE = "https://efa.vvo-online.de/VMSSL3/XSLT_COORD_REQUEST?jsonp=&boundingBox=&boundingBoxLU={minx}%3A{miny}%3AWGS84%5BDD.DDDDD%5D&boundingBoxRL={maxx}%3A{maxy}%3AWGS84%5BDD.DDDDD%5D&coordOutputFormat=WGS84%5BGGZHTXX%5D&type_1=STOP&outputFormat=json&inclFilter=1";
 
 const DISTRICT_CODES = [
 	"14511",
